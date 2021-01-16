eeb587c4553b51312e89ece4f4e831b2dde6abba
kiyohken2000 <www.broadwell.com@gmail.com>
2021-01-16 16:40:02 +0900

    master

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+const kenmofm = <a className="badge-link" href="https://kenmo.fm" target="_blank" rel="noreferrer noopener">Kenmo FM 567THz</a>
+const kenmoportal = <a className="badge-link" href="https://kenmo-fm.com" target="_blank" rel="noreferrer noopener">ケンモポータル</a>
+const kenmo = <a className="badge-link" href="http://leia.5ch.net/poverty/" target="_blank" rel="noreferrer noopener">嫌儲</a>
+
 export default function Footer() {
   return (
     <footer>
@@ -11,7 +15,9 @@ export default function Footer() {
             <Link to="/privacy">Privacy</Link>
           </li>
         </ul>
+        <p>Inspired by ❤️ {kenmofm} ❤️ {kenmoportal} ❤️ {kenmo}</p>
       </div>
     </footer>
   );
 }
+
