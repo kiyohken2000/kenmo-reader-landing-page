7f8f4f87ddaa36b48ec14d74435ccab6f95f0371
kiyohken2000 <www.broadwell.com@gmail.com>
2021-01-17 06:43:28 +0900

    master

--- src/pages/request.js
+++ src/pages/request.js
@@ -4,16 +4,27 @@ import Layout from '../components/Layout';
 const IndexPage = () => (
   <Layout>
     <section className="cta">
-      <div className="cta-content">
-        <div className="container">
-          <h2>
-            リクエスト
-          </h2>
-          <p>追加したいサイトをフィードバックウィジェットに送信してください</p>
-        </div>
-      </div>
-      <div className="overlay"></div>
-    </section>
+  <div className="cta-content">
+    <div className="container">
+      <h2>
+        みんなで最強のニュースリーダーアプリを作ろう
+      </h2>
+      <h3>
+        フィードバック（
+        <i className="fas fa-arrow-down"></i>
+        <i className="fas fa-arrow-right"></i>
+        の
+        <i className="fas fa-comment-alt"></i>
+        ）か
+        <i className="fab fa-twitter"></i>
+        であなたのお気に入りのサイトを教えてください
+        <br/><br/>
+        kenmo readerに対応しているウェブサイトをアプリに追加します
+      </h3>
+    </div>
+  </div>
+  <div className="overlay"></div>
+</section>
   </Layout>
 );
 
